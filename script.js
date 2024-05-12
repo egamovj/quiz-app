3aa2c7ee56c5a63d7419065c9361706ba5cb6269
egamovj <egamovj90@gmail.com>
2024-05-12 23:25:18 +0500

    added few effects

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const exitBtn = document.querySelector(".exit-btn");
 const main = document.querySelector(".main");
 const continueBtn = document.querySelector(".continue-btn");
 const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
 
 startBtn.onclick = () => {
   popupInfo.classList.add("active");
@@ -16,7 +17,9 @@ exitBtn.onclick = () => {
 };
 
 continueBtn.onclick = () => {
-    quizSection.classList.add('active')
-    popupInfo.classList.remove("active");
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
   main.classList.remove("active");
-}
+  quizBox.classList.add("active");
+
+};
