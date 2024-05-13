202f90c50d3781ba3527e2a45ddb04c8161204c1
egamovj <egamovj90@gmail.com>
2024-05-13 09:43:54 +0500

    added new questions

--- questions.js
+++ questions.js
@@ -23,91 +23,133 @@ let questions = [
   },
   {
     numb: 3,
-    question: "inputga malumotni kiritishni shart qilish uchun qaysi atributdan foydalanamiz?",
+    question:
+      "inputga malumotni kiritishni shart qilish uchun qaysi atributdan foydalanamiz?",
     answer: "B. required",
-    options: [
-      "A. disabled",
-      "B. required",
-      "C. default",
-      "D. type",
-    ],
+    options: ["A. disabled", "B. required", "C. default", "D. type"],
   },
   {
     numb: 4,
     question: "HTMLda havola yani link qaysi teg bilan yaratiladi?",
     answer: "A. a",
-    options: [
-        "A. a",
-        "B. p",
-        "C. i",
-        "D. button",
-    ],
+    options: ["A. a", "B. p", "C. i", "D. button"],
   },
   {
     numb: 5,
     question: "Qaysi teg yordamida tartibli ro'yhat yaratishimiz mumkin?",
     answer: "C. ol",
-    options: [
-        "A. td",
-        "B. ul",
-        "C. ol",
-        "D. tr",
-    ],
+    options: ["A. td", "B. ul", "C. ol", "D. tr"],
   },
   {
     numb: 6,
-    question: "link, meta, style va hokazo kabi bosh teglar qaysi teg ichida joylashishi kerak?",
+    question:
+      "link, meta, style va hokazo kabi bosh teglar qaysi teg ichida joylashishi kerak?",
     answer: "D. head",
-    options: [
-        "A. section",
-        "B. div",
-        "C. body",
-        "D. head",
-    ],
+    options: ["A. section", "B. div", "C. body", "D. head"],
   },
   {
     numb: 7,
-    question: "HTMLda qatorni bitta pastga tushirish uchun qaysi tegdan foydalanamiz?",
+    question:
+      "HTMLda qatorni bitta pastga tushirish uchun qaysi tegdan foydalanamiz?",
     answer: "B. br",
-    options: [
-        "A. hr",
-        "B. br",
-        "C. dl",
-        "D. dt",
-    ],
+    options: ["A. hr", "B. br", "C. dl", "D. dt"],
   },
   {
     numb: 8,
-    question: "img tegidagi qaysi atribut agar foydalanuvchi biron sababga ko'ra rasni ko'ra olmasa kerakli atribut rasm o'rniga matnni taqdim etadi?",
+    question:
+      "img tegidagi qaysi atribut agar foydalanuvchi biron sababga ko'ra rasni ko'ra olmasa kerakli atribut rasm o'rniga matnni taqdim etadi?",
     answer: "C. alt=''",
-    options: [
-        "A. src=''",
-        "B. href=''",
-        "C. alt=''",
-        "D. data=''",
-    ],
+    options: ["A. src=''", "B. href=''", "C. alt=''", "D. data=''"],
   },
   {
     numb: 9,
     question: "HTMLda text ostiga chizish uchun qaysi tegdan foydalanamiz?",
     answer: "A. u",
-    options: [
-        "A. u",
-        "B. i",
-        "C. del",
-        "D. sup",
-    ],
+    options: ["A. u", "B. i", "C. del", "D. sup"],
   },
   {
     numb: 10,
-    question: "Web-sahifada ko'p qatorli matn kiritish maydonini yaratish uchun qaysi tegdan foydalanamiz?",
+    question:
+      "Web-sahifada ko'p qatorli matn kiritish maydonini yaratish uchun qaysi tegdan foydalanamiz?",
     answer: "D. textarea",
+    options: ["A. div", "B. input", "C. placeholder", "D. textarea"],
+  },
+  {
+    numb: 11,
+    question: "Uzuk chiziqli border qanday yoziladi?",
+    answer: "A. dashed",
+    options: ["A. dashed", "B. solid", "C. dotted", "D. textarea"],
+  },
+  {
+    numb: 12,
+    question: "Quyidagilardan qaysi biri selector bo'la olmaydi?",
+    answer: "C. $h1",
+    options: ["A. .title", "B. h1", "C. $h1", "D. #title"],
+  },
+  {
+    numb: 13,
+    question: "Nuqtali border qanday yoziladi?",
+    answer: "D. dotted",
+    options: ["A. groove", "B. solid", "C. dashed", "D. dotted"],
+  },
+  {
+    numb: 14,
+    question: "RGB ranglarini qaysi sonlar orqali berishimiz mumkin?",
+    answer: "B. 0-255",
+    options: ["A. 0-256", "B. 0-255", "C. 0-188", "D. 1-200"],
+  },
+  {
+    numb: 15,
+    question:
+      "Qaysi css xossasi yordamida matnni katta kichik qilishimiz mumkin?",
+    answer: "C. font-size",
     options: [
-        "A. div",
-        "B. input",
-        "C. placeholder",
-        "D. textarea",
+      "A. font-weight",
+      "B. font-style",
+      "C. font-size",
+      "D. line-height",
     ],
   },
+  {
+    numb: 16,
+    question: "Sahifaga rasm joylash uchun qaysi tegdan foydalaniladi ?",
+    answer: "B. img",
+    options: ["A. pic", "B. img", "C. gif", "D. src"],
+  },
+  {
+    numb: 17,
+    question: "CSS ni necha xil yo'l bilan HTML faylga ulashimiz mumkin ?",
+    answer: "A. 3",
+    options: ["A. 3", "B. 4", "C. 2", "D. 1"],
+  },
+  {
+    numb: 18,
+    question:
+      "CSS da fontni qalin qilib yozish uchun qaysi qiymatdan foydalaniladi ?",
+    answer: "C. font-weight",
+    options: [
+      "A. font-variant",
+      "B. font-style",
+      "C. font-weight",
+      "D. font-size",
+    ],
+  },
+  {
+    numb: 19,
+    question:
+      "CSS da fontni qalin qilib yozish uchun qaysi qiymatdan foydalaniladi ?",
+    answer: "C. font-weight",
+    options: [
+      "A. font-variant",
+      "B. font-style",
+      "C. font-weight",
+      "D. font-size",
+    ],
+  },
+  {
+    numb: 20,
+    question: "font-weight ning qiymatlari qaysi oraliqqa to'g'ri keladi ?",
+    answer: "D. 100-900",
+    options: ["A. 1-10", "B. 1000-9000", "C. 0-900", "D. 100-900"],
+  },
 ];
-
